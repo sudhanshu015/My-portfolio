55799373e1fb36a117e2d987ae5a57d29496d974
sudhanshu015 <sudhanshukumar1139@gmail.com>
2026-06-01 03:23:01 +0530

    today 01/06/2026 upadate my portfolio

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text",{
+    strings: ["Front Developer","Data sciencest",],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
+// var typed = new Typed(".text", {
+//     strings: ["Frontend Developer", "YouTuber", "Web Developer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// });
